b97aff3811855e4a36db39de576a0f51310843c0
Ially Fabiane <iallyfabiane@gmail.com>
2022-03-02 13:50:19 -0300

    adicionando código

--- media.js
+++ media.js
@@ -7,3 +7,7 @@ let nota4 = 7.5;
 const notas = [10, 6.5, 8, 7.5];
 let media = (notas[0] + notas[1] + notas[2] + notas[3])/notas.length;
 console.log(media);
+
+const notas = [10, 6, 8];
+notas.push(7);
+console.log(notas);
