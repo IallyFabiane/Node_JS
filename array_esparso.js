87ec25c934712393d78659677f36a602b6f7e522
Ially Fabiane <iallyfabiane@gmail.com>
2022-03-02 14:44:01 -0300

    adicionando arquivo

---
+++ array_esparso.js
@@ -0,0 +1,8 @@
+const arrayVazia = [,,,];
+console.log(arrayVazia.length);
+console.log(arrayVazia[0]);
+console.log(arrayVazia[1]);
+console.log(arrayVazia[2]);
+arrayVazia.push(50);
+console.log(arrayVazia);
+console.log(arrayVazia.length);
