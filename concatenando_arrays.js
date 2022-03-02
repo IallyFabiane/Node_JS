0b4379709f1d50f232177991590c86d35731affd
Ially Fabiane <iallyfabiane@gmail.com>
2022-03-02 20:44:23 -0300

    adicionando arquivos

---
+++ concatenando_arrays.js
@@ -0,0 +1,4 @@
+const salaPhyton = ['Melisaa', 'Helena', 'Rodrigo'];
+const salaJavaScript = ['Ju', 'Leo', 'Raquel'];
+const salasUnificadas = salaPhyton.concat(salaJavaScript);
+console.log(salasUnificadas);
---
+++ lista_de_duas_dimensoes.js
@@ -0,0 +1,4 @@
+const listaDeAlunos = ['João', 'Juliana', 'Caio', 'Ana'];
+const mediasDosAlunos = [10, 7, 9, 6];
+let listaDeNotasEAlunos =[listaDeAlunos, mediasDosAlunos]; // lista de duas dimensões
+console.log(`${listaDeNotasEAlunos[0][0]}, sua média é ${listaDeNotasEAlunos[1][0]}.`);
