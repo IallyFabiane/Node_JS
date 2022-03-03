fe4e9fa75b84fbd4ec004a9f0e6a146f436b323c
Ially Fabiane <iallyfabiane@gmail.com>
2022-03-03 14:02:06 -0300

    adicionando arquivos

---
+++ strings_como-arrays.js
@@ -0,0 +1,13 @@
+const nome = "Alura";
+let nomeMaiusculas = "";
+
+for (let i = 0; i < nome.length; i++) {
+ nomeMaiusculas += nome[i].toUpperCase()
+}
+console.log(nomeMaiusculas); //ALURA
+
+const nomedoCurso = "Fundamentos de JS";
+const nomeDaPlataforma = " Alura";
+
+const nomeCompleto = nomedoCurso.concat(nomeDaPlataforma);
+console.log(nomeCompleto);  //Fundamentos de JS Alura
