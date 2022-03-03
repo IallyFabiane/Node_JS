d2aafc96e7e81016d646d864257336780fb09640
Ially Fabiane <iallyfabiane@gmail.com>
2022-03-03 09:50:48 -0300

    adicionando arquivos

---
+++ procurando_em_um_array.js
@@ -0,0 +1,14 @@
+const listaDeAlunos = ['João', 'Juliana','Caio', 'Ana'];
+const mediasDosAlunos = [10, 7, 9, 6];
+let listaDeNotasEAlunos =[listaDeAlunos, mediasDosAlunos]; // lista de duas dimensões
+
+const exibeNomeNota = (nomeDoAluno) => {
+    if (listaDeNotasEAlunos[0].includes(nomeDoAluno)) {
+        indice = listaDeNotasEAlunos[0].indexOf(nomeDoAluno);
+        return listaDeNotasEAlunos[0][indice] + " sua média é " + listaDeNotasEAlunos[1][indice];
+    } else {
+        return "O aluno não está cadastrado."
+    }
+}
+
+console.log(exibeNomeNota('Ana'));
