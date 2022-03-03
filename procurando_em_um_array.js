e774c5d13332b8ce1e6f98e625de87c4b011a854
Ially Fabiane <iallyfabiane@gmail.com>
2022-03-03 09:53:36 -0300

    ok

--- procurando_em_um_array.js
+++ procurando_em_um_array.js
@@ -4,7 +4,7 @@ let listaDeNotasEAlunos =[listaDeAlunos, mediasDosAlunos]; // lista de duas dime
 
 const exibeNomeNota = (nomeDoAluno) => {
     if (listaDeNotasEAlunos[0].includes(nomeDoAluno)) {
-        indice = listaDeNotasEAlunos[0].indexOf(nomeDoAluno);
+        let indice = listaDeNotasEAlunos[0].indexOf(nomeDoAluno);
         return listaDeNotasEAlunos[0][indice] + " sua média é " + listaDeNotasEAlunos[1][indice];
     } else {
         return "O aluno não está cadastrado."
