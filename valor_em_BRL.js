78843a0ebdd1f037ea442b7c4750b3b6f581742e
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-04-20 21:36:19 -0300

    Update valor_em_BRL.js

--- valor_em_BRL.js
+++ valor_em_BRL.js
@@ -1,5 +1,5 @@
 function somaPontoFlutuante() {
-console.log((0.1 + 0.2).toFixed(2).toString().replace('.', ','));
+console.log('R$', (0.1 + 0.2).toFixed(2).toString().replace('.', ','));
 }
 
 somaPontoFlutuante();
