3121a20d69afe6bde53e3d7211e5336c9dbd1ed7
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-04-20 21:12:38 -0300

    Add files via upload

---
+++ divisao_e_modulo.js
@@ -0,0 +1,6 @@
+const divisao = function (dividendo, divisor) {
+    console.log(`O resultado da divisão de ${dividendo} por ${divisor} é igual a`, dividendo / divisor, '.')
+    console.log(`O resto da divisão de ${dividendo} por ${divisor} é igual a`, dividendo % divisor, '.')
+}
+
+divisao(6, 3)
