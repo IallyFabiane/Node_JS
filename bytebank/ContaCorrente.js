47bb7f5675c2af5fe4f659289fa41804147b9417
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-04-28 15:03:10 -0300

    Update ContaCorrente.js

--- bytebank/ContaCorrente.js
+++ bytebank/ContaCorrente.js
@@ -1,5 +1,15 @@
+import { Cliente } from "./Cliente.js"
+
 export class ContaCorrente { //construindo uma classe
     agencia;
+    #cliente;
+
+    set cliente(novoValor) { //atribuindo valores ao cliente com a palavra-reservada set
+        if(novoValor instanceof Cliente) { //condicionando a atribuição do valor apenas se cliente for uma instância da classe Cliente
+            this.#cliente = novoValor;
+        }
+    }
+
     #saldo; //atributos seguido de # significa que são atributos privados(apenas a classe pode alterá-los)
     sacar(valor) {
         if(this.#saldo >= valor) { //condicionando o cliente que pode fazer uma operação de saque no banco
@@ -8,9 +18,13 @@ export class ContaCorrente { //construindo uma classe
         }
     }
     depositar(valor) { //condicionando a ação de depositar apenas se o saldo for positivo
-        if(valor > 0) {
+        if(valor <= 0) {
            return; //early return
         }
-        this.#saldo+= valor;
+        this.#saldo += valor;
+    }
+    transferir(valor, conta) {
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
     }
 }
