bfc28b8582fd37368e206f02b3aaecff14c88a7f
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-04-29 11:52:49 -0300

    Update index.js

--- bytebank/index.js
+++ bytebank/index.js
@@ -1,20 +1,23 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new Cliente(); //instanciando um objeto a partir de uma classe
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Ricardo", 11122233309); //instanciando um objeto a partir de uma classe
+const cliente2 = new Cliente("Alice", 44455566602); //instanciando um objeto a partir de uma classe
 
-const cliente2 = new Cliente(); //instanciando um objeto a partir de uma classe
-cliente2.nome = "Alice";
-cliente2.cpf = 44455566602;
-
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 0; //depósito inicial
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.depositar(100); //2º depósito
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+contaCorrenteRicardo.depositar(500); //2º depósito
 const valorSacado = contaCorrenteRicardo.sacar(50); //saque
+
+const conta2 = new ContaCorrente(102, cliente2);
+contaCorrenteRicardo.transferir(200, conta2);
 
 console.log(cliente1);
 console.log(cliente2);
 console.log(valorSacado);
+console.log(contaCorrenteRicardo);
+console.log(conta2);
+console.log(conta2.cliente);
+console.log(contaCorrenteRicardo.saldo);
+console.log(conta2.saldo);
+console.log(cliente1.cpf, cliente2.cpf);
+console.log(ContaCorrente.numeroDeContas);
