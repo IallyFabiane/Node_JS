8d27e3a823e3eea48552a54803d383663009794f
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-04-29 11:52:14 -0300

    Update Cliente.js

--- bytebank/Cliente.js
+++ bytebank/Cliente.js
@@ -1,4 +1,13 @@
 export class Cliente { //construindo uma classe 
     nome;
-    cpf; 
+    #cpf; 
+
+    get cpf() {
+        return this.#cpf;
+    }
+
+    constructor(nome, cpf) { //com o constructor é possível passar os valores de nome e cpf do cliente a ser criado, porém não é possível modificar o atributo privado
+        this.nome = nome;
+        this.#cpf = cpf;
+    }
 }
