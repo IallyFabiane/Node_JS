94e708d5eaea6028a0ed9fdb996caeb483dee4f8
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2022-04-29 21:30:22 -0300

    Update index.js

--- bytebank/index.js
+++ bytebank/index.js
@@ -9,15 +9,15 @@ contaCorrenteRicardo.depositar(500); //2º depósito
 const valorSacado = contaCorrenteRicardo.sacar(50); //saque
 
 const conta2 = new ContaCorrente(102, cliente2);
-contaCorrenteRicardo.transferir(200, conta2);
+let valor = 200;
+contaCorrenteRicardo.transferir(valor, conta2);
 
-console.log(cliente1);
-console.log(cliente2);
-console.log(valorSacado);
 console.log(contaCorrenteRicardo);
 console.log(conta2);
-console.log(conta2.cliente);
+console.log(ContaCorrente.numeroDeContas);
+console.log(cliente1);
+console.log(cliente2);
+console.log(cliente1.cpf);
+console.log(cliente2.cpf);
 console.log(contaCorrenteRicardo.saldo);
 console.log(conta2.saldo);
-console.log(cliente1.cpf, cliente2.cpf);
-console.log(ContaCorrente.numeroDeContas);
