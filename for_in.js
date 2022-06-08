d5503ea42a3f4708156a749bc6b8bd19c4c450ff
Ially Fabiane <iallyfabiane@gmail.com>
2022-06-08 16:39:32 -0300

    como acessar as chaves de um objeto com for in

--- for_in.js
+++ for_in.js
@@ -1,5 +1,19 @@
+//lendo os índices de um array
+
 const frutas = ['uva', 'melancia', 'banana'];
 
-for () {
-    
+for (let i in frutas) {
+    console.log(frutas[i]);
+}
+
+//lendo as chaves de um objeto
+
+const pessoa = {
+    nome: 'Ially',
+    sobrenome: 'Silva',
+    idade: 28
+}
+
+for (let k in pessoa) {
+    console.log(k, pessoa[k]);
 }
